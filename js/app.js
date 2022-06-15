842376faec2dcbc3f8dbad1a91bda17855fd2afc
mastero <castro.alejandro17@gmail.com>
2022-06-15 18:13:35 -0500

    Fusion Values Ethereal Spear Max & Min
    New DIV add on index.html

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ function dañoArma(){
     let fierceBerserkMax = ((3*(parseInt(Skill.value)+(2*parseInt(Arma.value)))) + parseInt(Nivel.value)/5);
     let groundShake = ((1.1*(parseInt(Skill.value)+(parseInt(Arma.value)))) + parseInt(Nivel.value)/5);
     let whirlwindThrow = (parseInt(Skill.value) + parseInt(Arma.value) + parseInt(Nivel.value)/5);
+    let etherealSpear = (parseInt(Skill.value) + 25 + (parseInt(Nivel.value)/5));
+    let etherealSpearMin = (((parseInt(Skill.value) + 25)/3) + (parseInt(Nivel.value)/5));
     let sd = (parseInt(Nivel.value)*0.2) + (parseInt(Magic.value)*7.395) + 46;
     let ice = (parseInt(Nivel.value)*0.2) + (parseInt(Magic.value)*2) + 12;
 
@@ -22,6 +24,12 @@ function dañoArma(){
     if(isNaN(dañoDist))
         dañoArma = "No puede esta vacio";
     document.getElementById("dañoDist").innerHTML = `Daño de Distancia: ${Math.round(dañoDist)}`;
+
+    if(isNaN(etherealSpear))
+        etherealSpear = "No puede esta vacio";
+    if(parseInt(Nivel.value) >= 23)
+    document.getElementById("etherealSpear").innerHTML = `Ethereal Spear: ${Math.round(etherealSpear)} Max`;
+    document.getElementById("etherealSpearMin").innerHTML = `------------------ ${Math.round(etherealSpearMin)} Min`;
 
     if(isNaN(fierceBerserkMax))
         fierceBerserkMax = "No puede esta vacio";
@@ -35,17 +43,17 @@ function dañoArma(){
 
     if(isNaN(groundShake))
         groundShake = "No puede esta vacio";
-    if(parseInt(Nivel.value) >= 40) 
+    if(parseInt(Nivel.value) >= 33) 
     document.getElementById("groundShake").innerHTML = `GroundShaker: ${Math.round(groundShake)} Max`;
     
     if(isNaN(whirlwindThrow))
         whirlwindThrow = "No puede esta vacio";
-    if(parseInt(Nivel.value) >= 37) 
+    if(parseInt(Nivel.value) >= 28) 
     document.getElementById("whirlwindThrow").innerHTML = `Whirlwind Throw: ${Math.round(whirlwindThrow)} Max`;
 
     if(isNaN(sd)) 
         sd = "No puede esta vacio";
-    if(parseInt(Magic.value) >= 14 && parseInt(Nivel.value) >= 45) 
+    if(parseInt(Magic.value) >= 15 && parseInt(Nivel.value) >= 45) 
     document.getElementById("sd").innerHTML = `Sudden Death: ${Math.round(sd)} Max`;
     
     if(isNaN(ice))
